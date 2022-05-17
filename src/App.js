8846af18bf95d3ea7125ddb5b162c4fe4115fab0
unknown <leonardoxavier092@gmail.com>
2022-05-17 20:26:25 -0300

    feat: input list e render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-function App() {
+import React from "react";
+import { v4 as uuid } from "uuid";
+
+import Lists from "./components/Lists/Lists";
+
+const App = () => {
+  const [lists, setLists] = React.useState([
+    { id: uuid(), title: "Groceries" },
+    { id: uuid(), title: "Movies" },
+    { id: uuid(), title: "Books" }
+  ]);
+
+  function addList(list) {
+    setLists([...lists, list]);
+  }
+
+  function removeList(id) {
+    const tempLists = lists.slice();
+    const index = tempLists.findIndex((list) => list.id === id);
+    tempLists.splice(index, 1);
+    setLists(tempLists);
+  }
+
   return (
     <div>
-      <h1>Todo-app</h1>
+      <Lists data={lists} onAddList={addList} onRemoveList={removeList} />
     </div>
   );
 }
