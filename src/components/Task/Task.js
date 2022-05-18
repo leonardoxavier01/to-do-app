a0e1d6e6bdccb4f5ec3e938fb343ddfca396e7a9
unknown <leonardoxavier092@gmail.com>
2022-05-18 00:03:47 -0300

    test: edite name task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,60 @@
 import React from "react";
 
 function Task(props) {
+  const taskTextRef = React.useRef();
+  const taskDoneRef = React.useRef();
+  const [isEditting, setIsEditting] = React.useState(false);
+  const [text, setText] = React.useState(props.task.text);
+  const [isDone, setIsDone] = React.useState(props.task.isDone);
+
+  function editButtonHandler() {
+    if (isEditting) {
+      props.onEdit({ id: props.task.id, text: text });
+      setIsEditting(false);
+    } else setIsEditting(true);
+  }
+
+  function editInputHandler() {
+    setText(taskTextRef.current.value);
+  }
+
+  function doneHandler() {
+    setIsDone(taskDoneRef.current.value);
+    props.onEdit({ id: props.task.id, isDone: isDone });
+  }
+
   function removeHandler() {
     return props.onRemove(props.id);
   }
 
   return (
     <div>
-      <input type="checkbox" checked={props.task.isDone ? true : false} />
-      <span>{props.task.text}</span>
+      <input
+        type="checkbox"
+        onChange={doneHandler}
+        ref={taskDoneRef}
+        checked={isDone ? true : false}
+      />
+
+      {isEditting && (
+        <form onSubmit={editButtonHandler}>
+          <input
+            type="text"
+            value={text}
+            ref={taskTextRef}
+            onChange={editInputHandler}
+          />
+          <button>save</button>
+        </form>
+      )}
+
+      {!isEditting && (
+        <span>
+          <span>{text}</span>
+          <button onClick={editButtonHandler}>edit</button>
+        </span>
+      )}
+
       <button onClick={removeHandler}>x</button>
     </div>
   );
