146685b78e58466a93d67ba7b8bbe9c633c3a8fa
unknown <leonardoxavier092@gmail.com>
2022-05-19 19:58:51 -0300

    style: create ListName e estilization in Lists

---
+++ src/components/ListName/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const ListName = ({children}) => {
+  return (
+    <div className='list_name'>{children}</div>
+  )
+}
+
+export default ListName
--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -2,7 +2,11 @@ import React from "react";
 import { v4 as uuid } from "uuid";
 
 import { BiPlus } from "react-icons/bi";
+import { BiAlignLeft } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import ListName from "../ListName";
+
+import { FaBuffer } from "react-icons/fa";
 
 function Lists(props) {
   const newListTitleRef = React.useRef();
@@ -24,14 +28,21 @@ function Lists(props) {
     <ul>
       {props.data.map((list, index) => (
         <li key={index}>
-          <Link to={"/" + list.id}>{list.title}</Link>
-          <button onClick={() => removeListHandler(index)}>x</button>
+          <Link className="name_list" to={"/" + list.id}>
+            <ListName>
+              <div className="margin_left">
+                <FaBuffer color={"#1a8a52"} />
+                <span>{list.title}</span>
+              </div>
+              <button className="button_add margin_rigth" onClick={() => removeListHandler(index)}>x</button>
+            </ListName>
+          </Link>
         </li>
       ))}
       <li>
         <form className="container_form" onSubmit={addListHandler}>
-          <input className="input" type="text" placeholder="New List" ref={newListTitleRef} />
-          <button className="button_add"><BiPlus size={17}/></button>
+          <input className="input" type="text" maxLength="16" placeholder="New List" ref={newListTitleRef} />
+          <button className="button_add"><BiPlus size={17} /></button>
         </form>
       </li>
     </ul>
