9e636fc3445fe6964987df8b6a84b8a76fc75bec
unknown <leonardoxavier092@gmail.com>
2022-05-20 18:26:29 -0300

    fix: styled.box.title

--- src/components/ListTitle/index.jsx
+++ src/components/ListTitle/index.jsx
@@ -6,11 +6,13 @@ import { BsPencilSquare } from 'react-icons/bs'
 const ListTitle = ({ children, onClickHandle }) => {
     return (
         <div className='container_title'>
-            <div className='testx'>
+            <div className='box_text_button'>
                 <FaBuffer color={"#1a8a52"} size={28} />
                 <h1 className='list_title'>{children}</h1>
+                <button className='button_add ' onClick={onClickHandle}>
+                    <BsPencilSquare color={"#1a8a52"} size={16} />
+                </button>
             </div>
-            <button className='button_add margin_left' onClick={onClickHandle}> <BsPencilSquare color={"#1a8a52"} size={16} /></button>
         </div>
     )
 }
