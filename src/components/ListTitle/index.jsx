83d002e1f670a2d2a562d52618cf6c8614d5da03
unknown <leonardoxavier092@gmail.com>
2022-05-20 23:38:15 -0300

    fix: clasname css

--- src/components/ListTitle/index.jsx
+++ src/components/ListTitle/index.jsx
@@ -9,7 +9,7 @@ const ListTitle = ({ children, onClickHandle }) => {
             <div className='box_text_button'>
                 <FaBuffer color={"#1a8a52"} size={28} />
                 <h1 className='list_title'>{children}</h1>
-                <button className='button_add ' onClick={onClickHandle}>
+                <button className='button_add' onClick={onClickHandle}>
                     <BsPencilSquare color={"#1a8a52"} size={16} />
                 </button>
             </div>
