09ba9e569fada8e379be1837e5a8f4ad12fd5f82
unknown <leonardoxavier092@gmail.com>
2022-05-22 16:13:41 -0300

    chore: create globalstyle

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,28 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500;600&display=swap');
 
-* {
+  * {
     margin: 0;
     padding: 0;
     clear: none;
     text-decoration: none;
     list-style: none;
-}
+    }
 
-body {
+  body {
     background-color: rgb(59, 59, 59);
     font-family: 'Noto Sans', sans-serif;
-}
+    }
+    
+    ::-webkit-scrollbar {
+    width: 8px; 
+    }
 
-::-webkit-scrollbar {
-    width: 8px;
-}
-
-::-webkit-scrollbar-button {
+    ::-webkit-scrollbar-button {
     width: 5px;
-}
+    }
 
-::-webkit-scrollbar-thumb {
+    ::-webkit-scrollbar-thumb {
     border-radius: 10px;
     background: #1a8a52;
     border: 2px solid rgba(0, 0, 0, 0.2);
-}
+    }
+`;
