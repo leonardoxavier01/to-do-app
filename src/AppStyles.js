a3f4b386c49d5daba4c09c673638048a60f631c3
unknown <leonardoxavier092@gmail.com>
2022-06-02 16:44:30 -0300

    style: converted vh to rem

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -14,8 +14,8 @@ export const ConatinerApp = styled.div`
     flex-direction: row;
     background-color: white;
     justify-content: center;
-    width: 70%;
-    height: 85vh;
+    width: 55rem;
+    height: 33rem;
     border-radius: 1%;
     -webkit-box-shadow: 0px 10px 23px -1px rgba(0, 0, 0, 0.43);
     -moz-box-shadow: 0px 10px 23px -1px rgba(0, 0, 0, 0.43);
@@ -25,16 +25,16 @@ export const ConatinerApp = styled.div`
 export const ListsStyled = styled.div`
     display: flex;
     flex-direction: column;
-    height: 66.56vh;
-    width: 40%;
+    height: 25rem;
+    width: 20rem;
     padding-left: 2rem;
     overflow: auto;
 `
 
 export const ListPageStyled = styled.div`
     background-color: rgb(218, 236, 232);
-    height: 61.44vh;
-    width: 60%;
+    height: 23rem;
+    width: 30rem;
     border-radius: 1%;
     padding: 2rem 2rem 4rem 5rem;
     overflow: auto;
